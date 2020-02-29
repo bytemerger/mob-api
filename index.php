6336a74fb75e863599890fcc9fbce19165890f31
francis <onyishif@gmail.com>
2020-02-29 12:42:45 +0100

    add routes

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <?php
+
+//require autoload file
+require __DIR__.'/vendor/autoload.php';
+
+//require redbean php
+require  __DIR__.'/helper/ORM/RedBeanPHP5_4_2/rb.php';
+
+//instantiate config
+new App\config\Config();
+
+
+$klein = new Klein\Klein();
+
+$klein->respond('GET','/persons',function (){
+    echo'we are in persons route';
+});
+$klein->respond('GET','/ticket/person/[:id]',function ($request){
+    echo "we are in ticket histry".$request->id;
+});
+$klein->dispatch();
