3d1528aba1739c48ce9ad76692b32be04ab9bd1a
francis <onyishif@gmail.com>
2020-03-01 23:19:40 +0100

    controller function to get person from model add a pagination and a start variable

--- app/controller/Person.php
+++ app/controller/Person.php
@@ -8,7 +8,17 @@ use App\model\Persons;
 class Person
 {
     //function to get the list of persons
-    public function getlist(){
+    public function getlist($page){
 
+        //variable to hold the next page to get
+        $start = $page * 10000;
+
+        //start from page one if page is not assigned
+        if (empty($page))
+        {
+            $start=1;
+        }
+        $list = new Persons();
+        echo json_encode($list->getList($start));
     }
 }
