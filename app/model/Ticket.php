3502ff369df614d1ed2f1679c74daa88c3841e16
francis <onyishif@gmail.com>
2020-03-02 17:10:23 +0100

    created getHistory function to access ticket_purchase_hist table get history for a specified id

--- app/model/Ticket.php
+++ app/model/Ticket.php
@@ -3,8 +3,31 @@
 
 namespace App\model;
 
+use App\config\Db;
+use RedBeanPHP\Facade as R;
+use App\config\Config;
 
 class Ticket
 {
 
+    public function getHistory($id)
+    {
+        //get the database connect
+        $db = (new Db())->getDb();
+        $sql= "SELECT * FROM ticket_purchase_hist WHERE purchased_by_id = ?";
+        $stmt=$db->prepare($sql);
+        $stmt->bindParam('1',$id);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+        //close database connection
+        (new Db())->closeDb();
+        return $result;
+
+        /*
+         * RedbeanPHP is not working with the ticket_purchase_hist
+         * it needs a primary key id(auto increment) to be able to convert table to bean
+         */
+        //return R::findAll('ticket_purchase_hist','purchased_by_id = ?',[$id]);
+    }
 }
